475391583fb07de226b9f7f9ecf2c8579d924ced
bell2lee <paperlee@kookmin.ac.kr>
2020-08-08 15:15:07 +0900

    Babel Settings

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Koa from 'koa';
+// const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = 'Hello Koa';
+});
+
+app.listen(4000, () => {
+    console.log('heurm server is listening to port 4000');
+});
